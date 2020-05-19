f04c66fba1d5197dbfc7cfb307cf2ecbf3889cac
Chris Smith <chris.b.smith@gmail.com>
2020-05-19 09:20:57 -0400

    update download url

--- Formula/gmenv.rb
+++ Formula/gmenv.rb
@@ -1,7 +1,7 @@
 class Gmenv < Formula
   desc "Grey Matter version manager inspired by tfenv"
   homepage "https://github.com/greymatter-io/gmenv"
-  url "https://github.com/greymatter-io/gmenv/archive/v0.2.0.tar.gz"
+  url "https://github.com/greymatter-io/gmenv/archive/0.2.0.tar.gz"
   sha256 "ddfb7b167e88da7bdf05f0f56dfb8a607a6c50e43787b5d42d1ca4373f8e6343"
   head "https://github.com/greymatter-io/gmenv.git"
 
