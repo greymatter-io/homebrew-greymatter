b56d1332394ad6495d454b56fb16858863008b20
Chris Smith <chris.b.smith@gmail.com>
2020-05-21 21:58:14 -0400

    test saving versions

--- Formula/gmenv.rb
+++ Formula/gmenv.rb
@@ -12,6 +12,10 @@ class Gmenv < Formula
   def install
     prefix.install ["bin", "libexec", "lib"]
 #     prefix.install "lib" if build.head?
+
+    mkdir_p "#{etc}/gmenv/versions" if File.directory?("#{etc}/gmenv/versions")
+    mkdir_p "versions"
+    ln_s "#{etc}/gmenv/versions", "versions"
   end
   
   test do
