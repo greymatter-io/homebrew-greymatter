1ee21104e00f41fb122a9399ddac3a53a956f10f
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-29 16:51:48 -0400

    Brew formula update for gmcat version v0.0.14

--- Formula/gmcat.rb
+++ Formula/gmcat.rb
@@ -2,16 +2,16 @@
 class Gmcat < Formula
   desc "GmCat is a tool to interact with The GreyMatter Service Mesh's Catalog"
   homepage "https://github.com/greymatter-io/gmcat"
-  version "0.0.9"
+  version "0.0.14"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/greymatter-io/gmcat/releases/download/v0.0.9/gmcat_0.0.9_darwin_amd64.tar.gz"
-    sha256 "79d3e90e7d24a410a083b25ef90a613ad3a550f69a45e20c5a784243467d15d4"
+    url "https://github.com/greymatter-io/gmcat/releases/download/v0.0.14/gmcat_0.0.14_darwin_amd64.tar.gz"
+    sha256 "64a453c2f2ec7a0bb4b34ab377e66f1dea1e17adf3483307853ee4b902cd62ed"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/greymatter-io/gmcat/releases/download/v0.0.9/gmcat_0.0.9_linux_amd64.tar.gz"
-      sha256 "715cf972a651258f070130af1536ae760fb8d7dc0a58858ae619614aa903b092"
+      url "https://github.com/greymatter-io/gmcat/releases/download/v0.0.14/gmcat_0.0.14_linux_amd64.tar.gz"
+      sha256 "05e16b27950b1d84246325f4f46d8abf3494033039c28fe70fcbfe34c5effdd0"
     end
   end
 
