7e8eba442530556bbaa85629cc18f64a7a76f651
Chris Smith <chris.smith@greymatter.io>
2021-05-27 15:07:41 -0400

    use correct sha (#19)

--- Formula/gmenv.rb
+++ Formula/gmenv.rb
@@ -2,7 +2,7 @@ class Gmenv < Formula
   desc "Grey Matter version manager inspired by tfenv"
   homepage "https://github.com/greymatter-io/gmenv"
   url "https://github.com/greymatter-io/gmenv/archive/0.3.5.tar.gz"
-  sha256 "df6f7ee990c81259fa0f0779624c679c4eaa05f42ffc93badf4ffa6b0350eebe"
+  sha256 "cc4c5a36be9101adb188e1b94abd32e149c6e60d065cb8afeffa3bd4f96df150"
   head "https://github.com/greymatter-io/gmenv.git"
 
   bottle :unneeded
